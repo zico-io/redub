6656bcf489a6af12d6e6d4e38282ff6af8a1dcfe
Nico Zamora <dev@zico.xyz>
2024-04-16 19:30:24 -0400

    Change default styles to use CTA styling.

--- components/CustomButton.jsx
+++ components/CustomButton.jsx
@@ -6,10 +6,10 @@ const CustomButton = ({ title, handlePress, containerStyles, textStyles, isLoadi
     <TouchableOpacity
         onPress={handlePress}
         activeOpacity={0.7}
-        className={`bg-secondary rounded-xl min-h-[62px] justify-center items-center ${containerStyles} ${isLoading ? 'opacity-50' : ''}`}
+        className={`bg-action rounded-xl min-h-[62px] justify-center items-center ${containerStyles} ${isLoading ? 'opacity-50' : ''}`}
         disabled={isLoading}
     >
-        <Text className={`text-primary font-fsemibold text-lg ${textStyles}`}>
+        <Text className={`text-secondary font-fsemibold text-lg ${textStyles}`}>
             {title}
         </Text>
     </TouchableOpacity>
